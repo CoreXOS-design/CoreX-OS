011090d1094fbcc9ef728dea5d28422b8fb132cd
Andre Roets <aroets@stafflion.com>
2026-02-25 09:48:40 +0200

    Fixed main

--- resources/views/layouts/nexus-sidebar.blade.php
+++ resources/views/layouts/nexus-sidebar.blade.php
@@ -8,8 +8,6 @@
         $nexusSection = 'document-library';
     } elseif ($currentPath === 'nexus' || $currentPath === 'nexus/') {
         $nexusSection = 'dashboard';
-    } elseif (str_starts_with($currentPath, 'nexus/documents')) {
-        $nexusSection = 'documents';
     } elseif (str_starts_with($currentPath, 'nexus/compliance')) {
         $nexusSection = 'compliance';
     } elseif (str_starts_with($currentPath, 'nexus/supervision')) {
@@ -74,17 +72,6 @@
             </svg>
             <span>Dashboard</span>
         </a>
-
-        {{-- Documents --}}
-        @if(!$user || $user->canAccessNexusSection('documents'))
-        <a href="{{ route('nexus.documents') }}" class="nexus-nav-item {{ $nexusSection === 'documents' ? 'active' : '' }}">
-            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor">
-                <path stroke-linecap="round" stroke-linejoin="round" d="M19.5 14.25v-2.625a3.375 3.375 0 0 0-3.375-3.375h-1.5A1.125 1.125 0 0 1 13.5 7.125v-1.5a3.375 3.375 0 0 0-3.375-3.375H8.25m2.25 0H5.625c-.621 0-1.125.504-1.125 1.125v17.25c0 .621.504 1.125 1.125 1.125h12.75c.621 0 1.125-.504 1.125-1.125V11.25a9 9 0 0 0-9-9Z" />
-            </svg>
-            <span>Documents</span>
-            <svg class="nexus-chevron" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor"><path stroke-linecap="round" stroke-linejoin="round" d="m8.25 4.5 7.5 7.5-7.5 7.5" /></svg>
-        </a>
-        @endif
 
         {{-- Compliance --}}
         @if(!$user || $user->canAccessNexusSection('compliance'))
