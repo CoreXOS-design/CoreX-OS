72c08aead9ee8e4d4d553d5ecbfa9cecb116147f
Andre Roets <aroets@stafflion.com>
2026-04-14 14:40:43 +0200

    fix

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -25,6 +25,7 @@ return Application::configure(basePath: dirname(__DIR__))
                 'admin_or_bm' => \App\Http\Middleware\AdminOrBranchManager::class,
                 'auth.portal_capture' => \App\Http\Middleware\AuthenticatePortalCapture::class,
                 'permission' => \App\Http\Middleware\CheckPermission::class,
+                'owner_only' => \App\Http\Middleware\OwnerOnly::class,
         ]);
 
         $middleware->validateCsrfTokens(except: [
