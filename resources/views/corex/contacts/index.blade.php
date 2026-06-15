d30fa74dfee5e10edebbade374af9eb8358440b1
Andre Roets <a.roets12@gmail.com>
2026-06-15 16:20:08 +0200

    fix

--- resources/views/corex/contacts/index.blade.php
+++ resources/views/corex/contacts/index.blade.php
@@ -51,7 +51,7 @@
                 </div>
             </div>
             @endpermission
-            <button type="button" @click="showAdd = !showAdd" class="corex-btn-primary text-sm"
+            <button type="button" @click="showAdd = !showAdd" class="corex-btn-primary text-sm">
                 <svg xmlns="http://www.w3.org/2000/svg" class="w-4 h-4" fill="none" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2.5">
                     <path stroke-linecap="round" stroke-linejoin="round" d="M12 4v16m8-8H4"/>
                 </svg>
